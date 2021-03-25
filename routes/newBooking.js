9eb0be89a57c84b83c6564e4b69aa2fd4c534739
Jye Robinson <jye@jrobi.co>
2021-03-25 17:58:57 +1000

    Updated the booking route to now add the booking information to a mongo collection

--- routes/newBooking.js
+++ routes/newBooking.js
@@ -6,14 +6,13 @@ module.exports = function (app, db) {
         }
         //Grab the username and password from the request.
         booking = req.body;
-        console.log("Got a new booking");
-        res.send({ok: true})
+        console.log(booking)
         //Try to find if there is a user that has that username and password.
-        //const collection = db.collection('bookings');
-        //collection.insertOne(booking, (err, dbres) => {
-            //if (err) throw err;
-            //let num = dbres.insertedCount;
-            //res.send({num: num, err:null, ok: true})
-        //});
+        const collection = db.collection('bookings');
+        collection.insertOne(booking, (err, dbres) => {
+            if (err) throw err;
+            let num = dbres.insertedCount;
+            res.send({num: num, err:null, ok: true})
+        });
     });
 }
