a717f569f4cc56a529f71c8a60a459eef3c7eb7e
Jye Robinson <jye@jrobi.co>
2021-03-29 23:54:28 +1000

    added google recaptcha for spam/bot prevention. Added functionality so only valid recaptchas with booking requests will be added to the db.

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -1,13 +1,14 @@
 const db = require('../models');
 const Bookings = db.Bookings
+const fetch = require('node-fetch');
+
 
 module.exports = {
-    create(req, res) {
+    async create(req, res) {
         if (!req.body) {
             res.sendStatus(400)
             return;
         }
-        console.log(req.body)
 
         const booking = {
             firstName: req.body.firstName,
@@ -19,15 +20,28 @@ module.exports = {
             requests: req.body.requests
         }
 
-        Bookings.create(booking)
+        const secret = process.env.RECAPTCHA_SECRET_KEY;
+        const response = await fetch("https://www.google.com/recaptcha/api/siteverify", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `secret=${secret}&response=${req.body.recaptchaResponse}`
+        }).then((res) => res.json())
         .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "An error occurred whilst attempting to create a booking."
-            });
-        });
+            if (data.success === true) {
+
+                Bookings.create(booking)
+                .then(data => {
+                    res.send(data);
+                })
+                .catch(err => {
+                    res.status(500).send({
+                    message: err.message || "An error occurred whilst attempting to create a booking."
+                    });
+                });
+            }
+        }); 
     },
 
     list(req, res) {
--- routes/newBooking.js
+++ routes/newBooking.js
@@ -1,4 +1,5 @@
 const booking = require('../controllers/bookings.controller');
+
 module.exports = function (app) {
     //Route for adding a new booking.
     app.post('/api/booking', booking.create);
