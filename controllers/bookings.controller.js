56589debfb8576d149b408715679f34f449b6910
Jye Robinson <jye@jrobi.co>
2021-04-27 19:25:49 +1000

    Added a new route and model method to update a booking

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -1,14 +1,13 @@
 const db = require('../models');
 const Bookings = db.Bookings
 const fetch = require('node-fetch');
-const { JsonWebTokenError } = require('jsonwebtoken');
 
 module.exports.create = (req, res) => {
-  console.log(req.body);
+    console.log(req.body);
     if (!req.body.firstName) return res.sendStatus(400);
 
     const secret = process.env.RECAPTCHA_SECRET_KEY;
-    
+
     fetch("https://www.google.com/recaptcha/api/siteverify", {
         method: 'POST',
         headers: {
@@ -16,55 +15,78 @@ module.exports.create = (req, res) => {
         },
         body: `secret=${secret}&response=${req.body.recaptchaResponse}`
     }).then((res) => res.json())
-    .then(data => {
-        if (data.success === true) {
+        .then(data => {
+            if (data.success === true) {
 
-            Bookings.create(req.body)
-            .then(data => {
-                res.send(data);
-            })
-            .catch(err => {
-                return res.status(500).json({
-                message: err.message || "An error occurred whilst attempting to create a booking."
+                Bookings.create(req.body)
+                    .then(data => {
+                        res.send(data);
+                    })
+                    .catch(err => {
+                        return res.status(500).json({
+                            message: err.message || "An error occurred whilst attempting to create a booking."
+                        });
+                    });
+            } else {
+                return res.status(400).json({
+                    message: "Invalid Captcha"
                 });
-            });
-        } else {
-          return res.status(400).json({
-            message: "Invalid Captcha"
-          });
-        }
-    }); 
+            }
+        });
 }
 
-module.exports.list= (req, res) => {
+module.exports.list = (req, res) => {
     Bookings.findAll({
         where: {
             date: req.body.selectedDate
         }
     })
-    .then(data => {
-        res.send(data);
-    })
-    .catch(err => {
-        return res.status(500).json({
-            message: err.message || "An error occurred whilst trying to query the database."
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message || "An error occurred whilst trying to query the database."
+            });
         });
-    });
 }
 
 module.exports.remove = (req, res) => {
-    console.log(req.body.id)
     Bookings.destroy({
         where: {
             id: req.body.id
         }
     })
-    .then(() => {
-        return res.sendStatus(200);
+        .then(() => {
+            return res.sendStatus(200);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message || "An error occurred whilst trying to delete a booking from the database."
+            });
+        })
+}
+
+module.exports.findBooking = (req, res) => {
+    Bookings.findOne({
+        where: {
+            id: req.body.id
+        }
     })
-    .catch(err => {
-        return res.status(500).json({
-            message: err.message || "An error occurred whilst trying to delete a booking from the database."
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: err.message || "An error occurred whilst querying the database."
+            });
         });
+}
+
+module.exports.update = (req, res) => {
+    Bookings.update(req.body.data, {
+        where: {
+            id: req.body.id
+        }
     })
 }
--- routes/bookingRouter.js
+++ routes/bookingRouter.js
@@ -5,7 +5,9 @@ const booking = require('../controllers/bookings.controller');
 const validator = require('../middleware/verifyToken');
 const user = require('../controllers/user.controller');
 
-router.post('/booking', booking.create);
+router.post('/createBooking', booking.create);
+router.post('/booking', validator.validateToken, booking.findBooking);
+router.put('/booking', validator.validateToken, booking.update)
 router.post('/admin', validator.validateToken, booking.list);
 router.delete('/admin', validator.validateToken, booking.remove);
 router.post('/login', user.find);
