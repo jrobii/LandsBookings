e3b69e968a185f8dacf9fa040b76df83e70c442f
Jye Robinson <jye@jrobi.co>
2021-05-20 11:43:31 +1000

    Fixed some small issues

--- index.js
+++ index.js
@@ -16,12 +16,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Routes
 app.use('/api', require('./routes/bookingRouter'))
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'https://landshotelbookings.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next();
-  });
 
 server.listen(http, PORT);
