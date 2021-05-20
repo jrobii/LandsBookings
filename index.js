f2dd15cbe5f6ba385fa75663be92828cc9afdb41
Jye Robinson <jye@jrobi.co>
2021-05-20 12:16:56 +1000

    Fixed error in cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 const PORT=process.env.PORT
 
-app.use(cors({credentials: true, origin: 'https://landshotelbookings.com:443'}));
+app.use(cors({credentials: true, origin: 'https://landshotelbookings.com'}));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,7 +18,7 @@ app.use('/api', require('./routes/bookingRouter'))
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'https://landshotelbookings.com/');
+    res.header('Access-Control-Allow-Origin', 'https://landshotelbookings.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
